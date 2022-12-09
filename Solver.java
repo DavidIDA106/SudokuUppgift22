4906072917fda1ac82b403bdddce9b78c18f1056
Carrrlitoth <engman.carl00@gmail.com>
2022-12-09 13:01:27 +0100

    thefuck

---
+++ Solver.java
@@ -0,0 +1,48 @@
+public class Solver implements SudokuSolver{
+    
+    public Solver() {
+        
+    }
+
+    @Override
+    public boolean solve() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean legal(int digit, int row, int col) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void set(int row, int col, int digit) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void remove(int row, int col) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setMatrix(int[][] matrix) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int[][] getMatrix() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
