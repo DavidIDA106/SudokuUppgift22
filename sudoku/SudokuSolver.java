7c5493e4f9e8c8b9affb5812dc5a3b1880f72f75
Carwall1 <carl.waller98@gmail.com>
2022-12-14 10:35:04 +0100

    Hela koden

--- sudoku/SudokuSolver.java
+++ sudoku/SudokuSolver.java
@@ -1,9 +1,12 @@
 package sudoku;
-public interface SudokuSolver{
 
-    boolean solve();
-    boolean legal(int digit, int row, int col);
-    /**
+public interface SudokuSolver {
+
+	boolean solve();
+
+	boolean legal(int digit, int row, int col);
+
+	/**
 	 * Puts digit in the box row, col.
 	 * 
 	 * @param row   The row
@@ -12,8 +15,9 @@ public interface SudokuSolver{
 	 * @throws IllegalArgumentException if row, col or digit is outside the range
 	 *                                  [0..9]
 	 */
-    void set(int row, int col, int digit);
-    /**
+	void set(int row, int col, int digit);
+
+	/**
 	 * Removes the digit in the box row, col.
 	 * 
 	 * @param row   The row
@@ -22,25 +26,26 @@ public interface SudokuSolver{
 	 * @throws IllegalArgumentException if row, col or digit is outside the range
 	 *                                  [0..9]
 	 */
-    void remove(int row, int col);
+	void remove(int row, int col);
 
-    /**
-     * Empties the grid.
-      */
-    void clear();
-    /**
+	/**
+	 * Empties the grid.
+	 */
+	void clear();
+
+	/**
 	 * Fills the grid with the digits in m. The digit 0 represents an empty box.
 	 * 
 	 * @param m the matrix with the digits to insert
 	 * @throws IllegalArgumentException if m has the wrong dimension or contains
 	 *                                  values outside the range [0..9]
 	 */
-    void setMatrix(int[][] matrix);
+	void setMatrix(int[][] matrix);
 
-    /**
+	/**
 	 * Returns a matrix with the current values.
 	 * 
 	 * @return integer matrix with current values
 	 */
-    int[][] getMatrix();
+	int[][] getMatrix();
 }
